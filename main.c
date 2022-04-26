9fa6d7d87d7966a9b921a4ba83e7da5fc7111ff8
Frederico Bento <fredbento39@gmail.com>
2022-04-26 15:24:51 +0100

    Rewrite of DrawFullBoard

--- main.c
+++ main.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-void draw(int[3][3]);
+
+int menu(int);
+void startUltimateGame(int[9][3][3],int[3][3],int[3][3]);
+void draw(int[9][3][3],int, int);
 void drawAll(int[3][3], int[3][3], int[3][3]);
 void drawFullBoard(int[9][3][3]);
-int menu(int);
-void startUltimateGame(int[9][3][3]);
+int convertCoordinates(int, int);
 
 void main(void){
     int m9[9][3][3];
-    char *c[9];
+    int xWonSquares[3][3],oWonSquares[3][3];
 
     int i, j,k,f;
     int op; 
@@ -22,6 +24,17 @@ void main(void){
             }
         }
     }
+    
+    int count=1;
+        for(i=0;i<3;i++){
+            for(j=0;j<3;j++){
+                xWonSquares[i][j]=0;
+                oWonSquares[i][j]=0;
+            }
+        }
+        
+    
+    oWonSquares[1][1]=1;
 
     do{
         op = menu(op);
@@ -33,25 +46,14 @@ void main(void){
             printf("\n## Choose an option: ");
             scanf("%d", &f);
             if(f==1){
-                startUltimateGame(m9);
+                startUltimateGame(m9,xWonSquares,oWonSquares);
             }else if(f==2){
                 op=0;
             }
             break;
     }
-    }while(op!=0);
+    }while(op!=3);
     
-
- 
-
-
-
-    //m9[0][0][0]=1;
-    //m9[1][1][1]=0;
-
-
-    //drawAll(m,m2,m3);
-   // drawFullBoard(m9);
 
 }
 
@@ -69,96 +71,134 @@ int menu(int op){
 
 }
 
-//Draw the Ultimate Full Board
-void drawFullBoard(int m[9][3][3]){
-    int i,j,k;
-    for(k=0;k<9;k++){
-        for(i=0;i<3;i++){ //square
-            for(j=0;j<3;j++){ //line
-                if(m[k][i][j]==-1){
-                    printf("| |");
-                }else if(m[k][i][j]==0){
-                    printf("|O|",m[k][i][j]);
-                }else if(m[k][i][j]==1){
-                    printf("|X|",m[k][i][j]);
-                }
-                
-            }
-         if(i!=2)
-            printf("#");
-        }
-         printf("\n");
-         if(k==2 || k==5 || k==8 || k==9){
-            printf("# # # # # # # # # # # # # # #\n");
-         }
-    }
-}
-//draw 1
-void draw(int m[9][3][3],int x, int y){
-    int i, j;
-    int pos;
-    x=x-1;
-    y=y-1;
-    if(x==0){
-        if(y==0){
-            pos=0;
-        }else if(y==1){
-            pos=1;
-        }else if(y==2){
-            pos=2;
-        }
-    }else if(x==1){
-        
-    }
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3; j++){
-            
-            printf("|%d|", m[x-1][[i][j]);
-  
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-//draw all
-void drawAll(int m[3][3], int m2[3][3], int m3[3][3]){
-    int i, j;
-
-    for(i = 0; i < 3; i++){
-        for(j = 0; j < 3; j++){
-            
-            printf("|%d|", m[i][j]);
-            printf("|%d|", m2[i][j]);
-            printf("|%d|", m3[i][j]);
-  
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-void startUltimateGame(int m[9][3][3]){
+void startUltimateGame(int m[9][3][3],int xWonSquares[3][3],int oWonSquares[3][3]){
     int x,x1,y,y1;
-   
+    int flag=0;
     drawFullBoard(m);
     
     do{
+        flag=0;
         printf("\n -> X playing, chose the main square (ex: 1,2): ");
         scanf("%d,%d", &x, &y);
         if(x < 1 || x > 3 || y < 1 || y > 3){
             printf("\n -> Invalid square, try again\n");
+            flag=1;
+        }else{
+            if(xWonSquares[x-1][y-1]==1 || oWonSquares[x-1][y-1]==1){
+                printf("\n -> Invalid square, that square is full  try again\n");
+                flag=1;
+            }
         }
-    }while(x < 1 || x > 3 || y < 1 || y > 3);
+    }while(flag!=0);
     
     do{
+        flag=0;
+      draw(m,x,y);
       printf("\n -> X playing, chose the position to play (Curret Square (%d,%d)): ",x,y);
       scanf("%d,%d", &x1, &y1);  
       if(x1 < 1 || x1 > 3 || y1 < 1 || y1 > 3){
             printf("\n -> Invalid square, try again\n");
-        }
-    }while(x1 < 1 || x1 > 3 || y1 < 1 || y1 > 3);
+            flag=1;
+      }else {
+          if(m[convertCoordinates(x,y)][x-1][y-1]!=-1){
+              printf("\nThat position is occupied!");
+              flag=1;
+          }
+      }
+    }while(flag!=0);
 
     
+}
+
+//Bad made function to draw the full board
+void drawFullBoard(int m[9][3][3]){
+    int k=0,i=0,j=0,counter=0,last=k,saved=0;
+    int stop=0;
+    while(stop!=1){
+        for(j=0;j<3;j++){
+            if(m[k][i][j]==-1){
+                printf("| |");
+            }else if(m[k][i][j]==0){
+                printf("|o|");
+            }else if(m[k][i][j]==1){
+                printf("|x|");
+            }else {
+                printf("|%d|",m[k][i][j]);
+            }
+        }
+        k++;
+        if(k!=0 && k%3==0){
+            last=k;
+            k=saved;
+            printf("\n");
+            counter++;
+            if(counter%3==0){
+                if(counter!=9)
+                    printf("# # # # # # # # # # # # # # # # # \n");
+                k=last;
+                saved=last;
+                i++;
+            }
+        }else printf(" # ");
+        if(counter==9){
+            stop=1;
+        }
+        
+   }
+
+}
+    
+
+//draw 1
+void draw(int m[9][3][3],int x, int y){
+    int i, j;
+    int pos = convertCoordinates(x,y); 
+    
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 3; j++){
+             if(m[pos][i][j]==-1){
+                    printf("| |");
+                }else if(m[pos][i][j]==0){
+                    printf("|O|");
+                }else if(m[pos][i][j]==1){
+                    printf("|X|");
+                }
+  
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int convertCoordinates(int x, int y){
+    int pos;
+        
+    if(x==1){
+        if(y==1){
+            pos=0;
+        }else if(y==2){
+            pos=1;
+        }else if(y==3){
+            pos=2;
+        }
+     }else if(x==2){
+            if(y==1){   
+                pos=3;
+            }else if(y==2){
+                pos=4;
+            }else if(y==3){
+                pos=5;
+            }
+    }else if(x==3){
+            if(y==1){
+                pos=6;
+            }else if(y==2){
+                pos=7;
+            }else if(y==3){
+                pos=8;
+            }
+        }
+       
+    return pos;
 }
 
